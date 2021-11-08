d5202e14a9a100eb1cbf5d2671970f048c4b63c4
monocuro <monocuro.dev@gmail.com>
2021-11-09 07:39:44 +0900

    client: Implement Items & Helper pages

--- albertpointscompanion/client/src/pages/Helpers.tsx
+++ albertpointscompanion/client/src/pages/Helpers.tsx
@@ -1,8 +1,12 @@
-import React, { FC, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { FC, useEffect, useState, useMemo } from 'react';
 
 import { HelperSet, getHelperSets } from '@/apiClient';
 import BaseLayout from '@/layouts/BaseLayout';
+import SectionedPage, {
+  Section,
+  SectionNavItem,
+} from '@/layouts/SectionedPage';
+import IdLink from '@/components/IdLink';
 
 const Helpers: FC = () => {
   const [helperSets, setHelperSets] = useState<HelperSet[]>([]);
@@ -11,9 +15,57 @@ const Helpers: FC = () => {
     getHelperSets().then(setHelperSets);
   }, []);
 
+  const sectionNavItems: SectionNavItem[] = useMemo(() => {
+    if (!helperSets) return [];
+
+    return helperSets.map((set) => ({
+      name: set.name,
+      id: encodeURIComponent(set.name),
+      children: set.teams.map((team) => ({
+        name: team.name,
+        id: encodeURIComponent(team.name),
+        children: team.helpers.map((helper) => ({
+          name: helper.name,
+          id: encodeURIComponent(helper.name),
+        })),
+      })),
+    }));
+  }, [helperSets]);
+
   return (
     <BaseLayout>
-      <h1>Helpers</h1>
+      <SectionedPage navItems={sectionNavItems}>
+        <IdLink id="Helpers">
+          <h1>Helpers</h1>
+        </IdLink>
+
+        {helperSets.map((set, index) => (
+          <Section key={index}>
+            <IdLink id={set.name} style="underline">
+              <h2>{set.name}</h2>
+            </IdLink>
+            <p>{set.description}</p>
+
+            {set.teams.map((team, idx) => (
+              <div key={idx}>
+                <IdLink id={team.name}>
+                  <h3>{team.name}</h3>
+                </IdLink>
+                <p>{team.description}</p>
+
+                {team.helpers.map((helper, i) => (
+                  <div key={i}>
+                    <IdLink id={helper.name} style="block">
+                      <h4>{helper.name}</h4>
+                    </IdLink>
+                    <p>{helper.description}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </Section>
+        ))}
+      </SectionedPage>
     </BaseLayout>
   );
 };
--- albertpointscompanion/client/src/pages/Items.tsx
+++ albertpointscompanion/client/src/pages/Items.tsx
@@ -1,8 +1,12 @@
-import React, { FC, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { FC, useEffect, useState, useMemo } from 'react';
 
 import { ItemSet, getItemSets } from '@/apiClient';
 import BaseLayout from '@/layouts/BaseLayout';
+import SectionedPage, {
+  Section,
+  SectionNavItem,
+} from '@/layouts/SectionedPage';
+import IdLink from '@/components/IdLink';
 
 const Items: FC = () => {
   const [itemSets, setItemSets] = useState<ItemSet[]>([]);
@@ -11,9 +15,44 @@ const Items: FC = () => {
     getItemSets().then(setItemSets);
   }, []);
 
+  const sectionNavItems: SectionNavItem[] = useMemo(() => {
+    if (!getItemSets) return [];
+
+    return itemSets.map((set) => ({
+      name: set.name,
+      id: encodeURIComponent(set.name),
+      children: set.items.map((item) => ({
+        name: item.name,
+        id: encodeURIComponent(item.name),
+      })),
+    }));
+  }, [itemSets]);
+
   return (
     <BaseLayout>
-      <h1>Items</h1>
+      <SectionedPage navItems={sectionNavItems}>
+        <IdLink id="Items">
+          <h1>Items</h1>
+        </IdLink>
+
+        {itemSets.map((set, index) => (
+          <Section key={index}>
+            <IdLink id={set.name} style="underline">
+              <h2>{set.name}</h2>
+            </IdLink>
+            <p>{set.description}</p>
+
+            {set.items.map((item, idx) => (
+              <div key={idx}>
+                <IdLink id={item.name} style="block">
+                  <h3>{item.name}</h3>
+                </IdLink>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </Section>
+        ))}
+      </SectionedPage>
     </BaseLayout>
   );
 };
