596581061f2ed4b14f0a5a42acf29f97e465a76d
monocuro <monocuro.dev@gmail.com>
2021-11-27 16:26:22 +0900

    api: Cache APIs

--- albertpointscompanion/api/views.py
+++ albertpointscompanion/api/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from django.forms.models import model_to_dict
+from django.views.decorators.cache import cache_page
 
 from .models import (
     MarkdownPages,
@@ -12,11 +13,14 @@ from .models import (
     Helper
 )
 
+DEFAULT_CACHE_DURATION = 60 * 5 # seconds
+
 # TODO: All resources are ordered by id but ideally, the order should be maintained separately
 
 # Markdown Pages
 
 AVAILABLE_MD_PAGES = ['home', 'getting_started', 'commands', 'items', 'dungeons']
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_markdown(request, page_name):
     if page_name not in AVAILABLE_MD_PAGES:
         return HttpResponseNotFound('Document not found')
@@ -26,45 +30,45 @@ def get_markdown(request, page_name):
 
 
 # Commands
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_command_sets(request):
     categories = CommandCategory.objects.all().order_by('id')
     data = [{**model_to_dict(category), 'commands': list(category.command_set.values().order_by('id'))}
             for category in categories]
     return JsonResponse(data, safe=False)
 
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_command_categories(request):
     data = list(CommandCategory.objects.values().order_by('id'))
     return JsonResponse(data, safe=False)
 
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_commands(request):
     data = list(Command.objects.values().order_by('id'))
     return JsonResponse(data, safe=False)
 
 
 # Items
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_item_sets(request):
     categories = ItemCategory.objects.all().order_by('id')
     data = [{**model_to_dict(category), 'items': list(category.item_set.values().order_by('id'))}
             for category in categories]
     return JsonResponse(data, safe=False)
 
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_item_categories(request):
     data = list(ItemCategory.objects.values().order_by('id'))
     return JsonResponse(data, safe=False)
 
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_items(request):
     data = list(Item.objects.values().order_by('id'))
     return JsonResponse(data, safe=False)
 
 
 # Helpers
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_helper_sets(request):
     groups = HelperGroup.objects.all().order_by('id')
     data = [
@@ -79,15 +83,17 @@ def get_helper_sets(request):
     ]
     return JsonResponse(data, safe=False)
 
-
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_helper_groups(request):
     data = list(HelperGroup.objects.values().order_by('id'))
     return JsonResponse(data, safe=False)
 
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_helper_teams(request):
     data = list(HelperTeam.objects.values().order_by('id'))
     return JsonResponse(data, safe=False)
 
+@cache_page(DEFAULT_CACHE_DURATION)
 def get_helpers(request):
     data = list(Helper.objects.values().order_by('id'))
     return JsonResponse(data, safe=False)
